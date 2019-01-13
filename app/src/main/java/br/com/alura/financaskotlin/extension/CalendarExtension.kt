43713c30b7c372ba28ff83048fb475c16555c7d8
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-01-13 19:17:17 -0200

    Ajustando imports.

--- app/src/main/java/br/com/alura/financaskotlin/extension/CalendarExtension.kt
+++ app/src/main/java/br/com/alura/financaskotlin/extension/CalendarExtension.kt
@@ -1,7 +1,8 @@
 package br.com.alura.financaskotlin.extension
 
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Locale
 
 fun Calendar.formataParaBrasileiro(): String {
     val formatoBrasileiro = "dd/MM/yyyy"
